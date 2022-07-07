6db765def1cbb585966dab4637e66ea1677cf57a
he24x <he24.2004@gmail.com>
2022-07-07 13:53:39 +0300

    Menu discontinued interface

--- app/src/main/java/com/example/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declarations
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, menuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
