4c6116851463744974ded1ffd48e1530670a1221
lupsavlad <lupsavlad51@gmail.com>
2022-07-14 13:25:17 +0300

    reservations and cos2

--- app/src/main/java/com/example/restaurantapp/registerActivity.java
+++ app/src/main/java/com/example/restaurantapp/registerActivity.java
@@ -2,7 +2,9 @@ package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class registerActivity extends AppCompatActivity {
@@ -14,5 +16,13 @@ public class registerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(registerActivity.this, menuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/restaurantapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/restaurantapp/ui/dashboard/DashboardFragment.java
@@ -23,8 +23,8 @@ import com.example.restaurantapp.databinding.FragmentDashboardBinding;
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
-    ImageView button1;
-    Button button2,button3,button4,button5,button6,button7,button8;
+//    ImageView button1;
+    Button button1, button2,button3,button4,button5,button6,button7,button8;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
